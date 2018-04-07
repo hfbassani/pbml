5a7cb4d5f0ee8c88925e37cc8e6216564bf8ad65
Pedro Henrique Magalhães Braga <pedromagalhaes.hb@gmail.com>
2018-04-06 21:51:12 -0300

    final: SSSOM | results | parameters | shell scripts

--- phmb4/py_scripts/run-eval.sh
+++ phmb4/py_scripts/run-eval.sh
@@ -1,7 +1,7 @@
-python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray4-l01 -r 500 -o pray4/pray4-l01 -p ../Parameters/pray4_0 -n ../Parameters/parametersNameHybrid
-python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray4-l05 -r 500 -o pray4/pray4-l05 -p ../Parameters/pray4_0 -n ../Parameters/parametersNameHybrid
-python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray4-l10 -r 500 -o pray4/pray4-l10 -p ../Parameters/pray4_0 -n ../Parameters/parametersNameHybrid
-python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray4-l25 -r 500 -o pray4/pray4-l25 -p ../Parameters/pray4_0 -n ../Parameters/parametersNameHybrid
-python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray4-l50 -r 500 -o pray4/pray4-l50 -p ../Parameters/pray4_0 -n ../Parameters/parametersNameHybrid
-python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray4-l75 -r 500 -o pray4/pray4-l75 -p ../Parameters/pray4_0 -n ../Parameters/parametersNameHybrid
-python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray4-l100 -r 500 -o pray4/pray4-l100 -p ../Parameters/pray4_0 -n ../Parameters/parametersNameHybrid
+python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray5-l01 -r 500 -o pray5/pray5-l01 -p ../Parameters/pray5_0 -n ../Parameters/parametersNameHybrid
+python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray5-l05 -r 500 -o pray5/pray5-l05 -p ../Parameters/pray5_0 -n ../Parameters/parametersNameHybrid
+python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray5-l10 -r 500 -o pray5/pray5-l10 -p ../Parameters/pray5_0 -n ../Parameters/parametersNameHybrid
+python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray5-l25 -r 500 -o pray5/pray5-l25 -p ../Parameters/pray5_0 -n ../Parameters/parametersNameHybrid
+python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray5-l50 -r 500 -o pray5/pray5-l50 -p ../Parameters/pray5_0 -n ../Parameters/parametersNameHybrid
+python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray5-l75 -r 500 -o pray5/pray5-l75 -p ../Parameters/pray5_0 -n ../Parameters/parametersNameHybrid
+python sssom_accuracy_calc.py -t ../../Datasets/Realdata_3Times3FoldsExp_Test -i ../Models/SSSOM/pray5-l100 -r 500 -o pray5/pray5-l100 -p ../Parameters/pray5_0 -n ../Parameters/parametersNameHybrid
