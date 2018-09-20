9b6aeefbfd6a76713d3f96855e1f3308e7c02d97
heitorrapela <hrm@cin.ufpe.br>
2018-09-20 15:57:20 -0300

    Add .txt features read

--- phmb4/Models/Pytorch36/datasets.py
+++ phmb4/Models/Pytorch36/datasets.py
@@ -9,13 +9,21 @@ from torch.utils.data import Dataset
 
 class ArffDataset(Dataset):
 
-    def __init__(self, arff_path):
-        data, meta = arff.loadarff(arff_path)
-        data = pd.DataFrame(data, dtype=float)
+    def __init__(self,load_path = "",arff_path=""):
+        data = None
+        if(load_path == ""):
+            data, meta = arff.loadarff(arff_path)
+            data = pd.DataFrame(data, dtype=float)
+        else: 
+            data = pd.read_csv(load_path, sep=",", header=None)
+            data = pd.DataFrame(data, dtype=float)
 
         self.y = data.iloc[:, -1].values
 
         self.X = data.iloc[:, :-1]
+        
+        if(load_path != ""):
+            self.y = self.y.astype(int)
 
     def __getitem__(self, index):
         data = torch.tensor(self.X.iloc[index])
