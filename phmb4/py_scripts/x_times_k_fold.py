a35c3299cdb814d23e6c3ecea9ac6aa161c9180d
Pedro Magalhaes <pedromagalhaes.hb@gmail.com>
2018-11-20 16:42:07 -0300

    Updating scripts to allow analysis with WIP project

--- phmb4/py_scripts/x_times_k_fold.py
+++ phmb4/py_scripts/x_times_k_fold.py
@@ -9,36 +9,38 @@ from sklearn import preprocessing
 import os
 import utils
 
-def create_arff_file (path, fileName, data, meta):
-    arffFile = open(join(path, fileName), 'w+')
+
+def create_arff_file(path, file_name, data, meta):
+    arff_file = open(join(path, file_name), 'w+')
 
     labels = data['class']
     labels = np.array(labels)
 
-    arffFile.write("@relation {0}\n".format(meta.name))
+    arff_file.write("@relation {0}\n".format(meta.name))
 
     for i in xrange(len(meta.names())):
         attr = meta.names()[i]
         if attr != "class":
-            arffFile.write("@attribute {0} {1}\n".format(attr, utils.get_type(meta.types()[i])))
+            arff_file.write("@attribute {0} {1}\n".format(attr, utils.get_type(meta.types()[i])))
         else:
-            arffFile.write("@attribute {0} {{".format(attr))
+            arff_file.write("@attribute {0} {{".format(attr))
             sorted_labels = sorted(map(int, np.unique(labels)))
-            arffFile.write("{0}".format(",".join(map(str, sorted_labels))))
-            arffFile.write("}\n")
+            arff_file.write("{0}".format(",".join(map(str, sorted_labels))))
+            arff_file.write("}\n")
 
-    arffFile.write("@data\n")
+    arff_file.write("@data\n")
 
     data = np.array(data)
     for row in xrange(len(data)):
-        arffFile.write(",".join(map(str, data[row])) + "\n")
+        arff_file.write(",".join(map(str, data[row])) + "\n")
 
-def create_true_file (path, fileName, data, labels):
-    trueFile = open(join(path, fileName), 'w+')
+
+def create_true_file(path, file_name, data, labels):
+    true_file = open(join(path, file_name), 'w+')
 
     dim = data.shape[1]
 
-    trueFile.write("DIM={0};{1} TRUE\n".format(dim, fileName[:-5].upper()))
+    true_file.write("DIM={0};{1} TRUE\n".format(dim, file_name[:-5].upper()))
 
     unique_labels = sorted(map(int, np.unique(labels)))
     for label in unique_labels:
@@ -50,11 +52,12 @@ def create_true_file (path, fileName, data, labels):
         line += " ".join(map(str, newIndexes))
         line += "\n"
 
-        trueFile.write(line)
+        true_file.write(line)
 
-def create_xtimes_kfolds(xTimes, kFolds, folder, outputFolder):
-    if not os.path.isdir(outputFolder + "_Train"): os.mkdir(outputFolder + "_Train")
-    if not os.path.isdir(outputFolder + "_Test"): os.mkdir(outputFolder + "_Test")
+
+def create_xtimes_kfolds(x_times, k_folds, folder, output_folder):
+    if not os.path.isdir(output_folder + "_Train"): os.mkdir(output_folder + "_Train")
+    if not os.path.isdir(output_folder + "_Test"): os.mkdir(output_folder + "_Test")
 
     files = [f for f in listdir(folder) if isfile(join(folder, f))]
     files = sorted(files)
@@ -72,8 +75,8 @@ def create_xtimes_kfolds(xTimes, kFolds, folder, outputFolder):
             data = scaler.transform(data)
             labels = np.array(labels)
 
-            for x in xrange(xTimes):
-                kf = KFold(n_splits=kFolds, shuffle=True)
+            for x in xrange(x_times):
+                kf = KFold(n_splits=k_folds, shuffle=True)
                 fold = 0
                 for train, test in kf.split(data):
                     train_data = data[train]
@@ -84,21 +87,22 @@ def create_xtimes_kfolds(xTimes, kFolds, folder, outputFolder):
 
                     trainFile = pd.DataFrame(train_data)
                     trainFile['class'] = train_labels
-                    create_arff_file(path=outputFolder + "_Train", fileName="train_{0}_x{1}_k{2}.arff".format(file[:-5], x + 1, fold + 1),
+                    create_arff_file(path=output_folder + "_Train", file_name="train_{0}_x{1}_k{2}.arff".format(file[:-5], x + 1, fold + 1),
                                      data=trainFile, meta=meta)
-                    create_true_file(path=outputFolder + "_Train",
-                                     fileName="train_{0}_x{1}_k{2}.true".format(file[:-5], x + 1, fold + 1),
+                    create_true_file(path=output_folder + "_Train",
+                                     file_name="train_{0}_x{1}_k{2}.true".format(file[:-5], x + 1, fold + 1),
                                      data=train_data, labels=train_labels)
 
                     testFile = pd.DataFrame(test_data)
                     testFile['class'] = test_labels
-                    create_arff_file(path=outputFolder + "_Test", fileName="test_{0}_x{1}_k{2}.arff".format(file[:-5], x + 1, fold + 1),
+                    create_arff_file(path=output_folder + "_Test", file_name="test_{0}_x{1}_k{2}.arff".format(file[:-5], x + 1, fold + 1),
                                      data=testFile, meta=meta)
-                    create_true_file(path=outputFolder + "_Test",
-                                     fileName="test_{0}_x{1}_k{2}.true".format(file[:-5], x + 1, fold + 1),
+                    create_true_file(path=output_folder + "_Test",
+                                     file_name="test_{0}_x{1}_k{2}.true".format(file[:-5], x + 1, fold + 1),
                                      data=test_data, labels=test_labels)
 
                     fold += 1
+
 
 def create_true_files_from_path(folder):
     files = [f for f in listdir(folder) if isfile(join(folder, f))]
@@ -108,13 +112,14 @@ def create_true_files_from_path(folder):
         if ".arff" in file:
             data, meta = arff.loadarff(open(join(folder, file), 'rb'))
             data = pd.DataFrame(data)
-            labels = data.iloc[:,-1]
+            labels = data.iloc[:, -1]
             del data[data.columns[len(data.columns) - 1]]
 
             data = np.array(data)
             labels = np.array(labels)
 
             create_true_file(folder, "{0}.true".format(file[:-5]), data, labels)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', help='Input Directory', required=True)
@@ -128,4 +133,4 @@ outputFolder = args.o
 X = args.x
 K = args.k
 
-create_xtimes_kfolds(xTimes=X, kFolds=K, folder=folder, outputFolder=outputFolder)
+create_xtimes_kfolds(x_times=X, k_folds=K, folder=folder, output_folder=outputFolder)
