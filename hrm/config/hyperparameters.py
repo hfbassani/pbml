3038540052348f0b43e26269b753e4fdf2896256
heitorrapela <hrm@cin.ufpe.br>
2018-11-29 07:55:15 -0300

    Add cifar100 model

--- hrm/config/hyperparameters.py
+++ hrm/config/hyperparameters.py
@@ -1,18 +1,23 @@
 # Hyperparameters
 class Hyperparameters():
-    def __init__(self,dataset_name='cifar10'):
-        super(Hyperparameters, self).__init__()
+	def __init__(self,dataset_name='svhn'):
+		super(Hyperparameters, self).__init__()
 
-        self.dataset_name = dataset_name
-        self.dataset_path = 'datasets/' + dataset_name
-        self.model_path = 'trained_models/'
-        self.num_classes = 10
-        self.num_epochs =  1
-        self.batch_size = 100
-        self.learning_rate = 0.001
+		self.dataset_name = dataset_name
+		self.dataset_path = 'datasets/' + dataset_name
+		self.model_path = 'trained_models/'
+		self.num_classes = 10
+		self.num_epochs =  20
+		self.batch_size = 128
+		self.learning_rate = 0.01
 
-        ## Fashion Mnist
-        self.num_epochs = 3
+		## Fashion Mnist
+		self.num_epochs = 3
 
-        ## CIFAR10
-        self.num_epochs =  10
+		## CIFAR10
+		self.num_epochs =  10
+
+		## CIFAR100
+		self.num_classes = 100
+		self.learning_rate = 0.001
+		self.num_epochs =  50
