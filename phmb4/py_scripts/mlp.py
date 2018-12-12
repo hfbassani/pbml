73fd67ccc7ab2629fb86fd59e93ccf6860864ddc
Pedro Magalhaes <pedromagalhaes.hb@gmail.com>
2018-12-12 11:40:24 -0300

    mlp environment

--- phmb4/py_scripts/mlp.py
+++ phmb4/py_scripts/mlp.py
@@ -1,7 +1,6 @@
 import argparse
 from sklearn import neural_network
 from sklearn import metrics
-from sklearn import preprocessing
 import pandas as pd
 import numpy as np
 from scipy.io import arff
@@ -9,7 +8,8 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-def run_mlp(train_X, train_Y, test_X, test_Y, neurons=100, hidden_layers=1, lr=0.001, momentum=0.9,
+
+def run_mlp(train_x, train_y, test_x, test_y, neurons=100, hidden_layers=1, lr=0.001, momentum=0.9,
             mlp_epochs=200, activation='logistic', lr_decay='constant', solver='lbfgs'):
     clf = neural_network.MLPClassifier(hidden_layer_sizes=(neurons,) * hidden_layers, activation=activation,
                                        solver=solver, alpha=0.0001,
@@ -21,15 +21,23 @@ def run_mlp(train_X, train_Y, test_X, test_Y, neurons=100, hidden_layers=1, lr=0
                                        validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
                                        epsilon=1e-8)
 
-    clf.fit(train_X, train_Y)
+    clf.fit(train_x, train_y)
 
-    accuracy = metrics.accuracy_score(clf.predict(test_X), test_Y)
+    accuracy = metrics.accuracy_score(clf.predict(test_x), test_y)
 
     return accuracy
 
-def run (folder, paramsFolder, output, supervision):
-    testFolder = folder.replace("_Train", "_Test")
-    files = [f for f in listdir(folder) if isfile(join(folder, f))]
+
+def run(folder, params_folder, output, supervision, sup_prefix):
+    test_folder = folder.replace("_Train", "_Test")
+
+    if sup_prefix:
+        files = [f for f in listdir(folder) if isfile(join(folder, f)) and f.endswith(".arff") and
+                 not f.startswith(".") and f.startswith("sup_")]
+    else:
+        files = [f for f in listdir(folder) if isfile(join(folder, f)) and f.endswith(".arff") and
+                 not f.startswith(".") and not f.startswith("sup_")]
+
     files = sorted(files)
 
     mlp_acc = []
@@ -39,98 +47,97 @@ def run (folder, paramsFolder, output, supervision):
     mean_value_mlp = []
     std_value_mlp = []
 
-    datasetNames = []
+    dataset_names = []
 
     for file in files:
-        if file.endswith(".arff") and "sup_" in file:
-            mlp_acc.append([])
+        mlp_acc.append([])
 
-            train_X, meta_trainX = arff.loadarff(open(join(folder, file), 'rb'))
-            train_X = pd.DataFrame(train_X)
-            train_Y = train_X['class']
-            del train_X['class']
+        train_x, meta_train_x = arff.loadarff(open(join(folder, file), 'rb'))
+        train_x = pd.DataFrame(train_x)
+        train_y = train_x['class']
+        del train_x['class']
 
-            train_X = np.array(train_X)
-            train_Y = np.array(train_Y)
+        train_x = np.array(train_x)
+        train_y = np.array(train_y)
 
-            testFile = file.replace("train_", "test_")
-            testFile = testFile.replace("sup_", "")
+        test_file = file.replace("train_", "test_")
+        test_file = test_file.replace("sup_", "")
 
-            if not testFolder.endswith("Test") and not testFolder.endswith("Test/"):
-                if testFolder.endswith("/"):
-                    testFolder = testFolder[:-4]
-                else:
-                    testFolder = testFolder[:-3]
+        if not test_folder.endswith("Test") and not test_folder.endswith("Test/"):
+            if test_folder.endswith("/"):
+                test_folder = test_folder[:-4]
+            else:
+                test_folder = test_folder[:-3]
 
-            test_X, meta_testX = arff.loadarff(open(join(testFolder, testFile), 'rb'))
-            test_X = pd.DataFrame(test_X)
-            test_Y = test_X['class']
-            del test_X['class']
+        test_x, meta_test_x = arff.loadarff(open(join(test_folder, test_file), 'rb'))
+        test_x = pd.DataFrame(test_x)
+        test_y = test_x['class']
+        del test_x['class']
 
-            test_X = np.array(test_X)
-            test_Y = np.array(test_Y)
+        test_x = np.array(test_x)
+        test_y = np.array(test_y)
 
-            alldata = np.append(train_X, test_X, axis=0)
-            # scaler = preprocessing.MinMaxScaler()
-            # scaler.fit(alldata)
-            # alldata = scaler.transform(alldata)
+        alldata = np.append(train_x, test_x, axis=0)
 
-            train_X = alldata[:len(train_X)]
-            test_X = alldata[len(train_X):]
+        train_x = alldata[:len(train_x)]
+        test_x = alldata[len(train_x):]
 
-            params = open(paramsFolder, 'r')
-            params = np.array(params.readlines())
+        params = open(params_folder, 'r')
+        params = np.array(params.readlines())
 
-            for paramsSet in range(0, len(params), 8):
-                neurons = int(params[paramsSet])
-                hidden_layers = int(params[paramsSet + 1])
-                lr = float(params[paramsSet + 2])
-                momentum = float(params[paramsSet + 3])
-                mlp_epochs = int(params[paramsSet + 4])
-                activation = getActivation(int(params[paramsSet + 5]))
-                lr_decay = getDecay(int(params[paramsSet + 6]))
-                solver = getSolver(int(params[paramsSet + 7]))
-                mlp_acc[len(mlp_acc) - 1].append(run_mlp(train_X, train_Y, test_X, test_Y, neurons, hidden_layers, lr,
-                                       momentum, mlp_epochs, activation, lr_decay, solver))
+        for paramsSet in range(0, len(params), 8):
+            neurons = int(params[paramsSet])
+            hidden_layers = int(params[paramsSet + 1])
+            lr = float(params[paramsSet + 2])
+            momentum = float(params[paramsSet + 3])
+            mlp_epochs = int(params[paramsSet + 4])
+            activation = get_activation(int(params[paramsSet + 5]))
+            lr_decay = get_decay(int(params[paramsSet + 6]))
+            solver = get_solver(int(params[paramsSet + 7]))
+            mlp_acc[len(mlp_acc) - 1].append(run_mlp(train_x, train_y, test_x, test_y, neurons, hidden_layers, lr,
+                                                     momentum, mlp_epochs, activation, lr_decay, solver))
 
-            max_values_mlp.append(np.nanmax(mlp_acc[len(mlp_acc) - 1]))
-            index_set_mlp.append(np.nanargmax(mlp_acc[len(mlp_acc) - 1]))
+        max_values_mlp.append(np.nanmax(mlp_acc[len(mlp_acc) - 1]))
+        index_set_mlp.append(np.nanargmax(mlp_acc[len(mlp_acc) - 1]))
 
-            mean_value_mlp.append(np.nanmean(mlp_acc[len(mlp_acc) - 1]))
-            std_value_mlp.append(np.nanstd(mlp_acc[len(mlp_acc) - 1], ddof=1))
+        mean_value_mlp.append(np.nanmean(mlp_acc[len(mlp_acc) - 1]))
+        std_value_mlp.append(np.nanstd(mlp_acc[len(mlp_acc) - 1], ddof=1))
 
-            datasetNames.append(file[:-5])
+        dataset_names.append(file[:-5])
 
-            outputText = "{0}\nMLP: {1}({2})[{3}]\n\n".format(file, np.mean(mlp_acc[len(mlp_acc) - 1]), np.std(mlp_acc[len(mlp_acc) - 1], ddof=1), np.argmax(mlp_acc[len(mlp_acc) - 1]))
-            print outputText
+        print "{0}\nMLP: {1}({2})[{3}]\n\n".format(file,
+                                                   np.mean(mlp_acc[len(mlp_acc) - 1]),
+                                                   np.std(mlp_acc[len(mlp_acc) - 1], ddof=1),
+                                                   np.argmax(mlp_acc[len(mlp_acc) - 1]))
 
-    writeResults(output, supervision, "mlp", mlp_acc, max_values_mlp, index_set_mlp, mean_value_mlp,
-                 std_value_mlp, datasetNames)
+    write_results(output, supervision, "mlp", mlp_acc, max_values_mlp, index_set_mlp, mean_value_mlp,
+                  std_value_mlp, dataset_names)
 
-def writeResults(outputPath, supervision, method, accs, max_values, index_set, mean_value, std_value,
-                 datasetNames):
+
+def write_results(output_path, supervision, method, accs, max_values, index_set, mean_value, std_value, dataset_names):
     if supervision == 1.0:
-        outputFile = open(join(outputPath, "{0}-l100.csv".format(method)), 'w+')
+        output_file = open(join(output_path, "{0}-l100.csv".format(method)), 'w+')
     else:
-        outputFile = open(
-            join(outputPath, "{0}-l{1}.csv".format(method, ('%.2f' % (supervision)).split(".")[1])), 'w+')
+        output_file = open(
+            join(output_path, "{0}-l{1}.csv".format(method, ('%.2f' % supervision).split(".")[1])), 'w+')
 
     line = "max_value," + ",".join(map(str, max_values)) + "\n"
     line += "index_set," + ",".join(map(str, index_set)) + "\n"
     line += "mean_value," + ",".join(map(str, mean_value)) + "\n"
     line += "std_value," + ",".join(map(str, std_value)) + "\n\n"
 
-    line += "experiment," + ",".join(datasetNames) + "\n"
+    line += "experiment," + ",".join(dataset_names) + "\n"
 
     for i in range(len(accs[0])):
         line += str(i)
-        for j in range(len(datasetNames)):
+        for j in range(len(dataset_names)):
             line += "," + str(accs[j][i])
         line += "\n"
 
-    outputFile.write(line)
+    output_file.write(line)
 
-def getActivation(activation):
+
+def get_activation(activation):
     if activation == 1:
         return 'logistic'
     elif activation == 2:
@@ -138,7 +145,8 @@ def getActivation(activation):
     else:
         return 'relu'
 
-def getDecay(lr_decay):
+
+def get_decay(lr_decay):
     if lr_decay == 1:
         return 'constant'
     elif lr_decay == 2:
@@ -146,7 +154,8 @@ def getDecay(lr_decay):
     else:
         return 'adaptive'
 
-def getSolver(solver):
+
+def get_solver(solver):
     if solver == 1:
         return 'lbfgs'
     elif solver == 2:
@@ -154,18 +163,22 @@ def getSolver(solver):
     else:
         return 'adam'
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', help='Train Data Directory', required=True)
 parser.add_argument('-p', help='Parameters', required=True)
 parser.add_argument('-o', help='Output', required=True)
 parser.add_argument('-s', help='Percentage of Supervision', required=True, type=float)
+parser.add_argument('--sup', help='Sup_ prefix', action='store_false', required=False)
 args = parser.parse_args()
 
 folder = args.i
 params = args.p
 output = args.o
 supervision = args.s
+sup_prefix = args.sup
 
-if not os.path.isdir(output): os.mkdir(output)
+if not os.path.isdir(output):
+    os.mkdir(output)
 
-run(folder, params, output, supervision)
+run(folder, params, output, supervision, sup_prefix)
