5722fbb9c1d18c9fec45b08c53a50a1c33aa8de6
Pedro Henrique Magalhães Braga <pedromagalhaes.hb@gmail.com>
2019-01-07 14:47:49 -0300

    balanced sampling

--- phmb4/py_scripts/organize_dataset_with_given_supervision.py
+++ phmb4/py_scripts/organize_dataset_with_given_supervision.py
@@ -8,80 +8,109 @@ import random
 import utils
 
 
-def create_arff(arff_file_name, folder_path, output_path, supervision_r):
+def balanced_sampling(data, supervision_r):
+    unique_labels = data['class'].unique()
+    labels = data['class']
+
+    labeled_len = int(len(labels) * supervision_r)
+    sample_per_class = int(labeled_len / len(unique_labels))
+
+    labeled_samples_indices = []
+
+    for target in unique_labels:
+        samples_target = data.index[data["class"] == target].tolist()
+        labeled_samples_indices += random.sample(samples_target, int(sample_per_class))
+
+    return labeled_samples_indices
+
+
+def create_arff(arff_file_name, folder_path, output_path, supervision_r, balanced):
     data, meta = arff.loadarff(open(join(folder_path, arff_file_name), 'rb'))
     data = pd.DataFrame(data)
 
     saved_labels = data['class'].unique()
-    labels = data['class']
+    labels = data['class'].values.astype(np.int)
 
-    while True:
-        rng = np.random.RandomState(random.randint(1, 20000))
-        random_unlabeled_points = rng.rand(len(labels)) >= supervision_r
-        curr_labels = np.copy(labels)
-        curr_labels[random_unlabeled_points] = str(999)
+    curr_labels = do_sample(balanced, data, labels, supervision_r)
 
-        if len(labels) - len(curr_labels[random_unlabeled_points]) >= len(labels) * supervision_r:
-            break
-
-    data['class'] = curr_labels
+    data['class'] = curr_labels.astype(np.str)
     curr_labels = set(curr_labels)
     utils.write_arff_file(arff_file_name, data, meta, output_path, curr_labels)
     data = data[data["class"] != str(999)]
     utils.write_arff_file("sup_" + arff_file_name, data, meta, output_path, saved_labels)
 
 
-def create_default(file_name, folder_path, output_path, supervision_r):
+def create_default(file_name, folder_path, output_path, supervision_r, balanced):
     data = pd.read_csv(join(folder_path, file_name), sep=",", header=None)
-    data = pd.DataFrame(data, dtype=float)
+    data = pd.DataFrame(data)
 
-    labels = data.iloc[:, -1].values.astype(np.int)
+    data = data.rename(columns={(len(data.columns) - 1): "class"})
+    labels = data['class']
 
-    while True:
-        rng = np.random.RandomState(random.randint(1, 20000))
-        random_unlabeled_points = rng.rand(len(labels)) >= supervision_r
-        curr_labels = np.copy(labels)
-        curr_labels[random_unlabeled_points] = 999
+    curr_labels = do_sample(balanced, data, labels, supervision_r)
 
-        if len(labels) - len(curr_labels[random_unlabeled_points]) >= len(labels) * supervision_r:
-            break
-
-    data[len(data.columns) - 1] = curr_labels
+    data['class'] = curr_labels.astype(np.int)
 
     data.to_csv(join(output_path, file_name), sep=',', index=False, header=False)
 
-    data = data[data[len(data.columns) - 1] != 999]
+    data = data[data['class'] != 999]
     data.to_csv(join(output_path, "sup_" + file_name), sep=',', index=False, header=False)
+
+
+def do_sample(balanced, data, labels, supervision_r):
+    if balanced:
+        labeled_samples_indices = balanced_sampling(data, supervision_r)
+        curr_labels = np.copy(labels)
+        curr_labels[~labels.index.isin(labeled_samples_indices)] = 999
+
+    else:
+        while True:
+            rng = np.random.RandomState(random.randint(1, 20000))
+            random_unlabeled_points = rng.rand(len(labels)) >= supervision_r
+            curr_labels = np.copy(labels)
+            curr_labels[random_unlabeled_points] = 999
+
+            if len(labels) - len(curr_labels[random_unlabeled_points]) >= len(labels) * supervision_r:
+                break
+
+    return curr_labels
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', help='Input Directory', required=True)
 parser.add_argument('-s', help='Percentage of Supervision', nargs='+', required=True, type=float)
+parser.add_argument('-b', help='Balanced Sampling', action='store_true', required=False)
+parser.add_argument('-n', help='Number of Datasets Sampled', required=False, type=int, default=1)
 args = parser.parse_args()
 
 folder_path = args.i
 folder_path = utils.check_directory(folder_path)
+balanced = args.b
 
 for supervision in args.s:
-    output_path = folder_path
+    for dataset_n_number in xrange(args.n):
+        output_path = folder_path
 
-    if output_path.endswith("/"):
-        output_path = output_path[:-1]
+        if output_path.endswith("/"):
+            output_path = output_path[:-1]
 
-    output_path += "S" + ('%.2f' % supervision).split(".")[1]
+        if args.n > 1:
+            output_path += "-n" + dataset_n_number + "-"
 
-    if not os.path.isdir(output_path):
-        os.mkdir(output_path)
+        output_path += "S" + ('%.2f' % supervision).split(".")[1]
 
-    files = [f for f in os.listdir(folder_path) if
-             os.path.isfile(join(folder_path, f)) and not f.startswith('.') and not f.endswith(".true")]
-    files = sorted(files)
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
 
-    for file in files:
-        if file.endswith(".arff"):
-            create_arff(arff_file_name=file, folder_path=folder_path,
-                        output_path=output_path, supervision_r=supervision)
-        else:
-            create_default(file_name=file, folder_path=folder_path,
-                           output_path=output_path, supervision_r=supervision)
+        files = [f for f in os.listdir(folder_path) if
+                 os.path.isfile(join(folder_path, f)) and not f.startswith('.') and not f.endswith(".true")]
+        files = sorted(files)
+
+        for file in files:
+            if file.endswith(".arff"):
+                create_arff(arff_file_name=file, folder_path=folder_path, output_path=output_path,
+                            supervision_r=supervision, balanced=balanced)
+            else:
+                create_default(file_name=file, folder_path=folder_path, output_path=output_path,
+                               supervision_r=supervision, balanced=balanced)
 
