d48dbcdc2cde4e962ca50e9efc09dbb9a4aa019b
heitorrapela <hrm@cin.ufpe.br>
2020-01-30 15:49:35 -0300

    Run glass tests

--- phmb4/Models/Pytorch36/generate.cpp
+++ phmb4/Models/Pytorch36/generate.cpp
@@ -7,7 +7,7 @@ int main()
 	
 	for(int i = 0 ; i < 500 ; i+=10)
 	{
-		cout << "tmux new-session -d \"conda activate deepunsupervised & python train_sssom.py --batch-size 32 --start-idx " << i << " --stop-idx " << i+10 << " -i ../../Parameters/batch_sssom_uci/diabetes/inputPathsReal_train_diabetes -t ../../Parameters/batch_sssom_uci/diabetes/inputPathsReal_test_diabetes -r ../../Parameters/batch_sssom_uci/diabetes/ -p ../../Parameters/sssom_0\"" << endl;
+		cout << "tmux new-session -d \"conda activate deepunsupervised & python train_sssom.py --batch-size 32 --start-idx " << i << " --stop-idx " << i+10 << " -i ../../Parameters/batch_sssom_uci/glass/inputPathsReal_train_glass -t ../../Parameters/batch_sssom_uci/glass/inputPathsReal_test_glass -r ../../Parameters/batch_sssom_uci/glass/results -p ../../Parameters/sssom_0\"" << endl;
 
 	}
 	
